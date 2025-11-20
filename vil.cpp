96a99e1b043ff2df3a5053124dd3ae27cc522e41
Vilena08 <vsaybuu@bk.ru>
2025-11-20 23:45:26 +0700

    vil.cpp

--- vil.cpp
+++ vil.cpp
@@ -1,8 +1,6 @@
 /********************
 * Author:Saibuu V.M *
-*                   *
-* Date: 06.11.2025  *
-*                   *
+* Date: 20.11.2025  *
 ********************/
 
 #include <iostream>
@@ -11,55 +9,54 @@
 
 using namespace std;
 
+constexpr int ALPHABET_SIZE = 26;
+
 int main() {
     ifstream inputFile("input.txt");
     if (!inputFile) {
-        cout << "Failed to open the input file." << endl;
+        cerr << "Error opening the input file." << endl;
         return 1;
     }
 
-    int letterFrequency[26];
-    for (int index = 0; index < 26; index++) {
-        letterFrequency[index] = 0;
-    }
+    int letterFrequency[ALPHABET_SIZE]{};
+    int pairLetterFrequency[ALPHABET_SIZE][ALPHABET_SIZE]{};
 
-    int pairLetterFrequency[26][26];
-    for (int firstLetterIndex = 0; firstLetterIndex < 26; firstLetterIndex++) {
-        for (int secondLetterIndex = 0; secondLetterIndex < 26; secondLetterIndex++) {
-            pairLetterFrequency[firstLetterIndex][secondLetterIndex] = 0;
-        }
-    }
-
+    int totalLetters = 0;
     int previousLetterIndex = -1;
     char currentChar;
 
-    while (inputFile >> currentChar) {
+    while (inputFile.get(currentChar)) {
         if (isalpha(currentChar)) {
             currentChar = tolower(currentChar);
             int currentLetterIndex = currentChar - 'a';
-
-            letterFrequency[currentLetterIndex]++;
+            ++letterFrequency[currentLetterIndex];
+            ++totalLetters;
 
             if (previousLetterIndex != -1) {
-                pairLetterFrequency[previousLetterIndex][currentLetterIndex]++;
+                ++pairLetterFrequency[previousLetterIndex][currentLetterIndex];
             }
 
             previousLetterIndex = currentLetterIndex;
         }
     }
 
-    cout << "Letter Frequencies:\n";
-    for (int index = 0; index < 26; index++) {
+    double frequencySum = 0.0;
+    cout << "Letter frequencies (%):\n";
+    for (int index = 0; index < ALPHABET_SIZE; index++) {
         if (letterFrequency[index] > 0) {
-            cout << (char)(index + 'a') << " = " << letterFrequency[index] << endl;
+            double freq = static_cast<double>(letterFrequency[index]) / totalLetters * 100.0;
+            cout << (char)(index + 'a') << ": " << fixed << setprecision(2) << freq << "%\n";
+            frequencySum += freq;
         }
     }
+    cout << "Total letters processed: " << totalLetters << ", Total sum of frequencies: " << fixed << setprecision(2) << frequencySum << "%\n";
 
-    cout << "\nTwo-Letter Pair Frequencies:\n";
-    for (int firstLetterIndex = 0; firstLetterIndex < 26; firstLetterIndex++) {
-        for (int secondLetterIndex = 0; secondLetterIndex < 26; secondLetterIndex++) {
+    cout << "\nPair Letter frequencies (%):\n";
+    for (int firstLetterIndex = 0; firstLetterIndex < ALPHABET_SIZE; firstLetterIndex++) {
+        for (int secondLetterIndex = 0; secondLetterIndex < ALPHABET_SIZE; secondLetterIndex++) {
             if (pairLetterFrequency[firstLetterIndex][secondLetterIndex] > 0) {
-                cout << (char)(firstLetterIndex + 'a') << (char)(secondLetterIndex + 'a') << " = " << pairLetterFrequency[firstLetterIndex][secondLetterIndex] << endl;
+                double freq = static_cast<double>(pairLetterFrequency[firstLetterIndex][secondLetterIndex]) / totalLetters * 100.0;
+                cout << (char)(firstLetterIndex + 'a') << (char)(secondLetterIndex + 'a') << ": " << fixed << setprecision(2) << freq << "%\n";
             }
         }
     }
